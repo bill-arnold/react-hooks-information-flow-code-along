ede40adfbe9ae6eaedc72e70aba73084d7cecfce
bill <bill.arnold@student.moringaschool.com>
2023-10-29 14:36:42 +0300

    Complete

--- src/Parent.js
+++ src/Parent.js
@@ -1,15 +1,22 @@
-import React, { useState } from "react";
-import { getRandomColor } from "./randomColorGenerator.js";
-import Child from "./Child";
+// import React, { useState } from 'react';
+import Child from './Child';
+import { getRandomColor } from '../randomColorGenerator';
 
 function Parent() {
   const randomColor = getRandomColor();
   const [color, setColor] = useState(randomColor);
+  const [childrenColor, setChildrenColor] = useState('#FFF');
+
+  function handleChangeColor(newChildColor) {
+    const newRandomColor = getRandomColor();
+    setColor(newRandomColor);
+    setChildrenColor(newChildColor);
+  }
 
   return (
     <div className="parent" style={{ backgroundColor: color }}>
-      <Child />
-      <Child />
+      <Child onChangeColor={handleChangeColor} color={childrenColor} />
+      <Child onChangeColor={handleChangeColor} color={childrenColor} />
     </div>
   );
 }
